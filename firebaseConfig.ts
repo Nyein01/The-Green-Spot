4c39a04ea31f4124c1e328ea7aaab3b9e082d65b
Nyein01 <nyeinswe999@gmail.com>
2026-01-21 16:14:49 +0700

    feat: Migrate data storage to Firebase
    Replaces local storage with Firebase Firestore for real-time data synchronization and persistence. Implements cloud-based storage for sales and inventory, and establishes real-time subscriptions for both. Includes initial seeding for default inventory items.

---
+++ firebaseConfig.ts
@@ -0,0 +1,19 @@
+import * as firebaseApp from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNbaGjHax-W4T8nvQ22J1PhL8ttu9xGpQ",
+  authDomain: "green-spot-41030.firebaseapp.com",
+  databaseURL: "https://green-spot-41030-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "green-spot-41030",
+  storageBucket: "green-spot-41030.firebasestorage.app",
+  messagingSenderId: "602147765172",
+  appId: "1:602147765172:web:c09fa24a0f32f953f890e1",
+  measurementId: "G-S2KMW3583Y"
+};
+
+// Initialize Firebase
+const app = firebaseApp.initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import * as firebaseApp from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRcReY37C7Ab0cjdUv638i34CSebnmokw",
+  authDomain: "the-green-spot-a1f66.firebaseapp.com",
+  projectId: "the-green-spot-a1f66",
+  storageBucket: "the-green-spot-a1f66.firebasestorage.app",
+  messagingSenderId: "741956597265",
+  appId: "1:741956597265:web:31ef1f65405a5c609f23b0",
+  measurementId: "G-D8D9VX0BSQ"
+};
+
+// Initialize Firebase
+const app = firebaseApp.initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
