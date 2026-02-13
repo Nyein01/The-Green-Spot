b071ede6cf15eaf747ba4e2583e2c3a4069b9d22
Nyein01 <nyeinswe999@gmail.com>
2026-02-13 17:23:21 +0700

    refactor: Remove unused Google auth from login
    The Google authentication flow and related providers have been removed from the login component and Firebase configuration. This simplifies the authentication process by focusing solely on manual username/password login.

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,7 +3,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // @ts-ignore
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBNbaGjHax-W4T8nvQ22J1PhL8ttu9xGpQ",
@@ -22,4 +22,3 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
